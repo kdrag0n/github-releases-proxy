d7d379fce783fee4630c64bea2ebaeecb462100a
Danny Lin <danny@kdrag0n.dev>
2021-04-21 00:20:14 -0700

    index: Add support for CORS

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 const ORIGIN_URL = "https://github.com/ProtonAOSP";
+
 // 2 months, in seconds
 const CACHE_TTL = 5259488;
+
+// Regex patterns for allowed CORS origins
+const CORS_ALLOWED_ORIGINS = [
+  /^protonaosp\.kdrag0n\.dev$/,
+  /^android-webinstall-kdrag0n\.vercel\.app$/,
+  /^android-webinstall-git-[a-z0-9\-_/]-kdrag0n\.vercel\.app$/,
+  /^android-webinstall-[a-z0-9]+-kdrag0n\.vercel\.app$/,
+];
 
 addEventListener('fetch', function(event) {
   let resp = handleRequest(event.request);
   return event.respondWith(resp);
 });
 
+function validateCors(origin) {
+  let originUrl = new URL(origin);
+  for (let pattern of CORS_ALLOWED_ORIGINS) {
+    if (pattern.test(originUrl.host)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 function parseRequest(request) {
+  if (request.method !== "GET") {
+    throw new Error("Invalid request method");
+  }
+
+  // Validate CORS, if necessary
+  let origin = request.headers.get("Origin")
+  if (origin !== null && !validateCors(origin)) {
+    throw new Error("Origin not allowed");
+  }
+
   let reqUrl = new URL(request.url);
   let reqParts = reqUrl.pathname.split("/").slice(1);
   // repo, tag, filename
-  if (reqParts.length != 3) {
+  if (reqParts.length !== 3) {
     throw new Error("Invalid request URL");
   }
 
   let [repo, tag, filename] = reqParts;
-  return `${ORIGIN_URL}/${repo}/releases/download/${tag}/${filename}`;
+  return {
+    url: `${ORIGIN_URL}/${repo}/releases/download/${tag}/${filename}`,
+    corsOrigin: origin,
+  };
 }
 
 async function handleRequest(request) {
-  let originUrl = parseRequest(request);
-  let originResp = await fetch(originUrl, {
+  let { url, corsOrigin } = parseRequest(request);
+  let originResp = await fetch(url, {
     cf: {
       // 2 months
       cacheTtl: CACHE_TTL,
@@ -32,5 +65,9 @@ async function handleRequest(request) {
   // Copy response and modify headers
   let clientResp = new Response(originResp.body, originResp);
   clientResp.headers.set("Cache-Control", `public, max-age=${CACHE_TTL}`);
+  if (corsOrigin !== null) {
+    clientResp.headers.set("Access-Control-Allow-Origin", corsOrigin);
+  }
+
   return clientResp;
 }
