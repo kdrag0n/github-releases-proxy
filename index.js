7e53d25d5f8b377814c1e64ad890c2ccf4274bc3
Danny Lin <danny@kdrag0n.dev>
2021-04-21 00:04:39 -0700

    index: Implement basic proxy

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+const ORIGIN_URL = "https://github.com/ProtonAOSP";
+// 2 months, in seconds
+const CACHE_TTL = 5259488;
+
+addEventListener('fetch', function(event) {
+  let resp = handleRequest(event.request);
+  return event.respondWith(resp);
+});
+
+function parseRequest(request) {
+  let reqUrl = new URL(request.url);
+  let reqParts = reqUrl.pathname.split("/").slice(1);
+  // repo, tag, filename
+  if (reqParts.length != 3) {
+    throw new Error("Invalid request URL");
+  }
+
+  let [repo, tag, filename] = reqParts;
+  return `${ORIGIN_URL}/${repo}/releases/download/${tag}/${filename}`;
+}
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  let originUrl = parseRequest(request);
+  let originResp = await fetch(originUrl, {
+    cf: {
+      // 2 months
+      cacheTtl: CACHE_TTL,
+      cacheEverything: true,
+    },
+  });
+
+  // Copy response and modify headers
+  let clientResp = new Response(originResp.body, originResp);
+  clientResp.headers.set("Cache-Control", `public, max-age=${CACHE_TTL}`);
+  return clientResp;
 }
