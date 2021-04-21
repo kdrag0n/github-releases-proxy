6a76328986a3384a160c1268daa1688635c525fb
Danny Lin <danny@kdrag0n.dev>
2021-04-21 00:49:14 -0700

    index: Handle errors with proper responses

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ const CORS_ALLOWED_ORIGINS = [
   /^android-webinstall-[a-z0-9]+-kdrag0n\.vercel\.app$/,
 ];
 
+class RequestError extends Error {
+  constructor(status, message) {
+    super(`HTTP ${status}: ${message}`);
+    this.status = status;
+    this.messageText = message;
+    this.name = this.constructor.name;
+  }
+}
+
 function validateCors(origin) {
   let originUrl = new URL(origin);
   for (let pattern of CORS_ALLOWED_ORIGINS) {
@@ -29,20 +38,20 @@ function validateCors(origin) {
 
 function parseRequest(request) {
   if (request.method !== "GET") {
-    throw new Error("Invalid request method");
+    throw new RequestError(405, "Invalid request method");
   }
 
   // Validate CORS, if necessary
   let origin = request.headers.get("Origin")
   if (origin !== null && !validateCors(origin)) {
-    throw new Error("Origin not allowed");
+    throw new RequestError(403, "Origin not allowed");
   }
 
   let reqUrl = new URL(request.url);
   let reqParts = reqUrl.pathname.split("/").slice(1);
   // repo, tag, filename
   if (reqParts.length !== 3) {
-    throw new Error("Invalid request URL");
+    throw new RequestError(404, "Invalid request URL");
   }
 
   let [repo, tag, filename] = reqParts;
@@ -56,7 +65,7 @@ function modifyResponse(response) {
   response.headers.set("Cache-Control", `public, max-age=${CACHE_TTL}`);
 }
 
-async function handleRequest(request) {
+async function respondAsset(request) {
   const cache = caches.default;
   let { url, corsOrigin } = parseRequest(request);
 
@@ -89,6 +98,24 @@ async function handleRequest(request) {
   }
 
   return clientResp;
+}
+
+async function handleRequest(request) {
+  try {
+    return await respondAsset(request);
+  } catch (e) {
+    // Handle RequestError exceptions with proper error responses
+    if (e instanceof RequestError) {
+      return new Response(e.messageText, {
+        status: e.status,
+        headers: {
+          "Content-Type": "text/plain;charset=UTF-8",
+        },
+      });
+    } else {
+      throw e;
+    }
+  }
 }
 
 addEventListener('fetch', function(event) {
