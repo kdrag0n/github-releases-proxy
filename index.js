a8597e7a19ed7d4de5530e97c0a62284cfe88a1a
Danny Lin <danny@kdrag0n.dev>
2021-04-21 00:41:54 -0700

    index: Cache GitHub asset responses

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// URL to fetch upstream assets from
 const ORIGIN_URL = "https://github.com/ProtonAOSP";
 
-// 2 months, in seconds
-const CACHE_TTL = 5259488;
+// Whether to cache assets from GitHub
+// Disable to conserve Workers cache quota
+const USE_CACHE = true;
+
+// Browser cache TTL, in seconds
+const CACHE_TTL = 5259488; // 2 months
 
 // Regex patterns for allowed CORS origins
 const CORS_ALLOWED_ORIGINS = [
@@ -52,19 +57,38 @@ function parseRequest(request) {
   };
 }
 
+function modifyResponse(response) {
+  response.headers.set("Cache-Control", `public, max-age=${CACHE_TTL}`);
+}
+
 async function handleRequest(request) {
+  const cache = caches.default;
   let { url, corsOrigin } = parseRequest(request);
-  let originResp = await fetch(url, {
-    cf: {
-      // 2 months
-      cacheTtl: CACHE_TTL,
-      cacheEverything: true,
-    },
-  });
+
+  let originResp = undefined;
+  if (USE_CACHE) {
+    // Try cache first
+    originResp = await cache.match(request);
+  }
+  if (originResp === undefined) {
+    // Fetch from origin
+    originResp = await fetch(url);
+
+    if (USE_CACHE) {
+      // Tee the stream for both the client and caching readers to use
+      let bodies = originResp.body.tee();
+      originResp = new Response(bodies[0], originResp);
+      let cacheResp = new Response(bodies[1], originResp);
+
+      // Set TTL for caching purposes
+      modifyResponse(cacheResp);
+      await cache.put(request, cacheResp);
+    }
+  }
 
   // Copy response and modify headers
   let clientResp = new Response(originResp.body, originResp);
-  clientResp.headers.set("Cache-Control", `public, max-age=${CACHE_TTL}`);
+  modifyResponse(clientResp);
   if (corsOrigin !== null) {
     clientResp.headers.set("Access-Control-Allow-Origin", corsOrigin);
   }
